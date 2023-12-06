6a837256f76773ee1ade7cf60fb73c86645db259
eriplume <eripi840@gmail.com>
2023-12-06 12:31:04 +0900

    :lipstick: Add: profile用コンポーネント作成 #8
    userImage, menuで切り分け

---
+++ app/components/ui/UserImage.tsx
@@ -0,0 +1,21 @@
+import { UserImageProps } from '@/types/user';
+import Image from 'next/image';
+
+const UserImage = ({ image, name }: UserImageProps) => {
+  const imageUrl = image ?? '/default_profile.png'; // デフォルト画像のパス
+  const userName = name ?? 'Unknown User';
+
+  return (
+    <div className="flex items-center cursor-pointer">
+      <Image
+        src={imageUrl}
+        className="w-8 h-8 rounded-full"
+        alt={userName}
+        width={32}
+        height={32}
+      />
+    </div>
+  );
+};
+
+export default UserImage;
