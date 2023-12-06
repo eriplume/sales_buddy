6b4ea5a03485a6bcc939aa76854c9d208adadc3f
eriplume <eripi840@gmail.com>
2023-12-06 12:34:03 +0900

    :sparkles: Feat: ヘッダーからsingin/ouが可能 #8
    Session状態によって表示分岐,dropdownメニューからログアウト

--- app/components/base/UserStatus.tsx
+++ app/components/base/UserStatus.tsx
@@ -1,27 +1,21 @@
 "use client"
 import { useSession } from "next-auth/react";
-import { LoginButton } from "../LoginButton";
-import Image from 'next/image'
+import  LoginButton  from "../LoginButton";
+import ProfileDropdown from "./ProfileDropdown";
 
-const UserStatus = () => {
+export default function UserStatus () {
   const { data: session } = useSession();
 
-  const imageUrl = session?.user?.image ?? '../../public/btn_login_base.png'; // デフォルトのプロフィール画像へのパス
-  const userName = session?.user?.name ?? 'Unknown User';
-
   return session ? (
-    <div className="flex items-center">
-      <Image
-        src={imageUrl}
-        className="w-8 h-8 rounded-full"
-        alt={userName}
-        width={32} // Image コンポーネントに必要な width
-        height={32} // Image コンポーネントに必要な height
-      />
-    </div>
+    <ProfileDropdown
+      image={session.user.image} 
+      name={session.user.name} 
+    />
   ) : (
-    <LoginButton />
-  );
-};
+    <div className="w-24 sm:w-32">
+      <LoginButton/>
+    </div>
+  )
+}
 
-export default UserStatus;
+
