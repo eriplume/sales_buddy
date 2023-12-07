a9ad6e72993c71bbbd8daaddba9b366df6a8e28f
eriplume <eripi840@gmail.com>
2023-12-07 13:05:19 +0900

    :sparkles: Feat: モバイル用ヘッダーメニュー作成 #8

--- app/components/base/Header.tsx
+++ app/components/base/Header.tsx
@@ -1,12 +1,13 @@
-import { Bars3Icon, CloudIcon } from "@heroicons/react/24/outline";
+import { CloudIcon } from "@heroicons/react/24/outline";
 import UserStatus from "./UserStatus";
+import DrawerMenu from "./DrawerMenu";
 
 export default function Header() {
   return (
     <header className="sticky top-0 text-gray-600 body-font bg-white">
       <div className="container mx-auto flex flex-wrap p-5 items-center justify-between">
         <div className="flex items-center">
-        <Bars3Icon className="w-6 h-6 md:hidden mr-2" /> {/* モバイルビューでのみアイコン表示 */}
+        <span className="md:hidden"><DrawerMenu/></span>
           <div className="flex items-center">
             <span className="mx-1 text-xl font-bold">sales buddy</span>
             <CloudIcon className="w-6 h-6 font-bold" />
@@ -15,6 +16,5 @@ export default function Header() {
         <UserStatus />
       </div>
     </header>
-
   )
 }
