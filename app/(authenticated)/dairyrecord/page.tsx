876a4b05b251d09b0f1af336f76994cede4d6a92
eriplume <eripi840@gmail.com>
2023-12-07 13:07:09 +0900

    :construction: Add: dairyrecordページ仮 #8
    遷移確認のため作成,後にコンテンツ作成

---
+++ app/(authenticated)/dairyrecord/page.tsx
@@ -0,0 +1,10 @@
+export default function DairyRecordPage() {
+    return (
+      <>
+      <div className="flex justify-center items-center">
+        <p>売上記録ページです</p>
+      </div>
+      </>
+    )
+  }
+  
