8f21fb9fe81ac3332a380d9dec6bc498364e0e54
eriplume <eripi840@gmail.com>
2023-12-09 18:52:04 +0900

    :sparkles: Feat: 入力,加算処理,現在値の出力機能作成 #4

--- app/components/page/DairyRecord/RecordInputForm.tsx
+++ app/components/page/DairyRecord/RecordInputForm.tsx
@@ -1,68 +1,86 @@
-'use client'
+import { z } from 'zod';
+import { useForm } from '@mantine/form';
+import { zodResolver } from 'mantine-form-zod-resolver';
 import useCalculationStore from '@/store/calculationStore';
-import { NumberInput, Select, Button, Group } from '@mantine/core';
+import { NumberInput, Select, Button } from '@mantine/core';
+import { PlusCircleIcon } from "@heroicons/react/24/solid";
+
+type FormValues = {
+  number: number;
+  amount: number;
+  customer: string;
+};
+
+const schema  = z.object({
+  number: z.number().min(1,  { message: '0より大きな値を入力してください' }),
+  amount: z.number().min(1, { message: '0より大きな値を入力してください' }),
+  customer: z.string().refine(val => val !== '', { message: '客層を選択してください' }),
+});
 
 export default function RecordInputForm () {
-
   const { addToTotal, options } = useCalculationStore();
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    // フォームデータの取得
-    const formData = new FormData(event.currentTarget);
-    const amount = parseFloat(formData.get('amount') as string);
-    const number = parseFloat(formData.get('number') as string);
-    const customer = formData.get('customer') as string;
-    
-    addToTotal({ amount, number, customer });
-    console.log(amount)
-    console.log(number)
-    console.log(customer)
+  const form = useForm({
+    initialValues: {
+      number: 0,
+      amount: 0,
+      customer: '',
+    },
+    validate: zodResolver(schema),
+  });
 
-
-    // フォームのリセット
-    event.currentTarget.reset();
+  const handleSubmit = (values: FormValues) => {
+    addToTotal(values);
+    form.reset();
   };
 
-    return (
-        <>
-        <div className="flex justify-center items-center w-full">
-        <form onSubmit={handleSubmit} className="w-full mx-auto px-2">
-        <NumberInput
-          label="点数"
-          name="number"
-          withAsterisk
-          min={0}
-          max={30}
-          suffix="点"
-          size="xs"
-        />
-        <NumberInput
-          label="金額"
-          name="amount" 
-          withAsterisk
-          min={0}
-          max={500000}
-          prefix="￥"
-          thousandSeparator=","
-          size="xs"
-          hideControls
-        />
-        <Select
-          label="客層"
-          withAsterisk
-          size="xs"
-          data={options.map((option) => ({
-            value: option.value.toString(),
-            label: option.label,
-          }))}
-        />
+  return (
+    <>
+      <div className="flex justify-center items-center w-full">
+        <form onSubmit={form.onSubmit(handleSubmit)} className='w-full mx-auto px-2'>
+          <NumberInput
+            {...form.getInputProps('number')}
+            label="点数"
+            name="number"
+            withAsterisk
+            min={0}
+            max={30}
+            suffix="点"
+            size="xs"
+          />
+          <NumberInput
+            {...form.getInputProps('amount')}
+            label="金額"
+            name="amount" 
+            withAsterisk
+            min={0}
+            max={500000}
+            prefix="￥"
+            thousandSeparator=","
+            size="xs"
+            hideControls
+          />
+          <Select
+            key={form.values.customer}
+            {...form.getInputProps('customer')}
+            label="客層"
+            name="customer" 
+            withAsterisk
+            size="xs"
+            data={options.map((option) => ({
+              value: option.value.toString(),
+              label: option.label,
+            }))}
+          />
 
-        <Group justify="flex-end" mt="md">
-          <Button type="submit">加算する</Button>
-        </Group>
-      </form>
+          <div className="flex justify-end mt-4">
+            <Button type="submit" variant="outline" color="gray" size="xs">
+              加算する
+              <PlusCircleIcon className="w-5 h-5 ml-1 text-blue-400" />
+            </Button>
+          </div>
+        </form>
       </div>
-        </>
-    )
+    </>
+  )
 }
