22dd797af47a016f0dfe5042d83adf8329e8ffef
eriplume <eripi840@gmail.com>
2023-12-10 12:48:42 +0900

    :art: Update: レスポンシブ修正 #4

--- app/(authenticated)/dairyrecord/page.tsx
+++ app/(authenticated)/dairyrecord/page.tsx
@@ -4,7 +4,7 @@ import Submit from "@/app/components/page/DairyRecord/Submit";
 export default function DairyRecordPage() {
     return (
       <>
-      <div className="flex flex-col md:flex-row justify-center items-center m-auto p-6 z-0 md:gap-x-6 max-w-3xl mx-auto">
+      <div className="flex flex-col md:flex-row justify-center items-center mx-auto p-6 z-0 md:gap-x-6 max-w-5xl my-10">
         <Caluculator/>
         <Submit/>
       </div>
--- app/components/base/Header.tsx
+++ app/components/base/Header.tsx
@@ -5,7 +5,7 @@ import DrawerMenu from "./DrawerMenu";
 export default function Header() {
   return (
     <header className="sticky top-0 text-gray-600 body-font bg-white z-50">
-      <div className="container mx-auto flex flex-wrap p-5 items-center justify-between">
+      <div className="container mx-auto flex flex-wrap p-6 items-center justify-between md:max-w-5xl">
         <div className="flex items-center">
         <span className="md:hidden"><DrawerMenu/></span>
           <div className="flex items-center">
