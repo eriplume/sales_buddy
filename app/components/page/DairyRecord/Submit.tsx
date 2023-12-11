938f1265334c0365607634056328eaae68a0e4d9
eriplume <eripi840@gmail.com>
2023-12-11 15:06:13 +0900

    :art: Update: 選択された日付の表示を追加 #4

--- app/components/page/DairyRecord/Submit.tsx
+++ app/components/page/DairyRecord/Submit.tsx
@@ -3,12 +3,14 @@ import { Fieldset } from '@mantine/core';
 import CurrentData from './CurrentData';
 import ClearButton from '../../ui/ClearButton';
 import SubmitButton from '../../ui/SubmitButton';
+import CurrentDate from './CurrentDate';
 
 export default function Submit() {
   return (
     <>
     <Fieldset legend="現在入力されている合計" className='w-full mt-3 max-w-lg' style={{ overflowX: 'auto' }}>
       <div className="flex flex-col space-y-4 w-full">
+        <CurrentDate/>
         <CurrentData/>
         <div className="flex justify-end mt-4 gap-3">
           <ClearButton />
