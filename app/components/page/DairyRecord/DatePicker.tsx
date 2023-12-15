e32e02c4b54b8580d8113bfb1aed0cdbce1c8d3a
eriplume <eripi840@gmail.com>
2023-12-15 17:28:14 +0900

    :zap: Update: 売上入力時すでに登録済みの日を選択不可に変更 #5

--- app/components/page/DairyRecord/DatePicker.tsx
+++ app/components/page/DairyRecord/DatePicker.tsx
@@ -1,13 +1,21 @@
 "use client"
 import useCalculationStore from '@/store/calculationStore';
+import useDashboardStore from '@/store/dashboardStore';
 import { DatePickerInput } from '@mantine/dates';
 import { rem } from '@mantine/core';
 import { CalendarIcon } from "@heroicons/react/24/outline";
+import { formatDate } from '@/utils/dateUtils';
 
 export default function Datepicker() {
   const { count, selectedDate, setSelectedDate } = useCalculationStore();
+  const { salesDates } = useDashboardStore((state) => ({ salesDates: state.salesDates }));
 
   const isDataEntered = count !== 0;
+
+  const excludeDate = (date: Date) => {
+    const formattedDate = formatDate(date);
+    return salesDates.includes(formattedDate);
+  };
 
   const icon = <CalendarIcon style={{ width: rem(18), height: rem(18) }}/>;
 
@@ -27,6 +35,7 @@ export default function Datepicker() {
       disabled={isDataEntered}
       valueFormat= "YYYY / MM / DD   ( ddd )"
       maxDate={new Date()}
+      excludeDate={excludeDate}
     />
   );
 }
