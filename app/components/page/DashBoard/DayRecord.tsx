54c8fa8c1f6c73449c305e006a9419ab421d05a2
eriplume <eripi840@gmail.com>
2023-12-15 16:05:01 +0900

    :lipstick: Add: モーダルコンポーネントの中身を作成 #5

--- app/components/page/DashBoard/DayRecord.tsx
+++ app/components/page/DashBoard/DayRecord.tsx
@@ -1,11 +1,6 @@
-type SalesRecord = {
-  average_spend: number
-  count: number
-  date: Date
-  set_rate: number
-  total_amount: number
-  total_number: number
-};
+import { SalesRecord } from "@/types/SalesRecord";
+import {  CurrencyYenIcon } from "@heroicons/react/24/outline";
+import { ShoppingBagIcon,UserIcon } from "@heroicons/react/24/solid";
 
 type DayRecordProps = {
   record: SalesRecord;
@@ -16,13 +11,26 @@ export default function DayRecord({ record }: DayRecordProps) {
   const formatCurrency = (amount :number) => {
     return new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(amount);
   }
-    return (
-      <>
-      <div className="p-4">
-        <div className="mb-2">売上金額：{formatCurrency(record.total_amount)}</div>
-        <div className="mb-2">セット率：{record.set_rate}</div>
-        <div className="mb-2">客単価：{formatCurrency(record.average_spend)}</div>
+
+  return (
+    <>
+      <div className="p-4 border-t">
+        <div className="mb-2 flex items-center text-gray-700">
+          <CurrencyYenIcon className="w-6 h-6 text-gray-500 mr-2" />
+          売上金額：
+          <span>{formatCurrency(record.total_amount)}</span>
+        </div>
+        <div className="mb-2 flex items-center text-gray-700">
+          <UserIcon className="w-6 h-6 text-gray-500 mr-2" />
+          客単価：
+          <span>{formatCurrency(record.average_spend)} （客数: {record.count}）</span>
+        </div>
+        <div className="mb-2 flex items-center text-gray-700">
+          <ShoppingBagIcon className="w-6 h-6 text-gray-500 mr-2" />
+          セット率：
+          <span>{record.set_rate} （点数: {record.total_number}）</span>
+        </div>
       </div>
-      </>
-    )
-  }
+    </>
+  )
+}
--- app/components/page/DashBoard/NotDayRecord.tsx
+++ app/components/page/DashBoard/NotDayRecord.tsx
@@ -7,11 +7,14 @@ export default function NotDayRecord() {
   const router = useRouter()
   return (
     <>
-      <div className="p-4">
-        <div className="mb-2">売上を記録しますか？</div>
-        <Button type="submit" variant="outline" color="gray" size="xs" onClick={() => router.push('/dairyrecord')}>
+      <div className="p-4 border-t">
+        <div className="mb-2">
+          <p>売上データがありません。</p>
+          <p>売上を記録しますか？</p>
+        </div>
+        <Button type="submit" variant="outline" color="gray" size="sm" onClick={() => router.push('/dairyrecord')}>
           記録しにいく
-          <CursorArrowRaysIcon className="w-5 h-5 ml-1 text-blue-400" />
+          <CursorArrowRaysIcon className="w-6 h-6 ml-1 text-blue-400" />
         </Button>
       </div>
     </>
