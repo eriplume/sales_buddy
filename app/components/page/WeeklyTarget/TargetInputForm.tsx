1257c7919feb85a15c1bcd36a0c7b324d7515343
eriplume <eripi840@gmail.com>
2023-12-19 14:21:42 +0900

    :lipstick: Add: targetエリアコンポーネント作成 #6

--- app/components/page/WeeklyTarget/TargetInputForm.tsx
+++ app/components/page/WeeklyTarget/TargetInputForm.tsx
@@ -1,14 +1,8 @@
-"use client"
 import { NumberInput } from '@mantine/core';
-import axios from 'axios';
-import { showSuccessNotification } from '@/utils/notifications';
-import { showErrorNotification } from '@/utils/notifications';
 import { useRouter } from 'next/navigation';
 import { z } from 'zod';
 import { useForm } from '@mantine/form';
 import { zodResolver } from 'mantine-form-zod-resolver';
-import { PlusCircleIcon } from "@heroicons/react/24/solid";
-import { Button } from '@mantine/core';
 
 type targetValues = {
   target: number;
@@ -31,39 +25,21 @@ export default function TargetInputForm() {
 
   const handleSubmit = async (values: targetValues) => {
     console.log(values)
-    // try {
-    //   const response = await axios.post(`/api/weekly_target`, { 
-    //     weekly_target: values
-    //   });
-
-    //   // response.dataから日付を取得
-    //   const date = response.data.weekly_target.date;
-    //   showSuccessNotification(`${date}の目標を登録しました`);
-    //   router.push('/dashboard');
-    // } catch (error) {
-    //   showErrorNotification('送信に失敗しました。もう一度お試しください。');
-    //   console.error("Failed to fetch", error);
-    // }
   };
-
 
   return (
     <>
       <div className="flex justify-center items-center w-full">
-        <form onSubmit={form.onSubmit(handleSubmit)} className='w-full mx-auto px-6'>
+        <form onSubmit={form.onSubmit(handleSubmit)} className='w-full'>
           <NumberInput
           {...form.getInputProps('target')}
+            label="目標金額（万）"
             placeholder="100万円"
             suffix="万円"
-            mt="md"
+            size="xs"
+            withAsterisk
+            min={0}
           />
-
-          <div className="flex justify-end mt-4">
-            <Button type="submit" variant="outline" color="gray" size="xs">
-              登録する
-              <PlusCircleIcon className="w-5 h-5 ml-1 text-blue-400" />
-            </Button>
-          </div>
         </form>
       </div>
     </>
