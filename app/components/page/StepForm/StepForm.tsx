b1492989a0c28340ffa6bab6687dfacf6b4764c6
eriplume <eripi840@gmail.com>
2023-12-20 22:19:10 +0900

    :zap: Feat: 目標登録のskip機能, ステップ順でのエラーハンドリングに修正 #25

--- app/components/page/StepForm/StepForm.tsx
+++ app/components/page/StepForm/StepForm.tsx
@@ -8,6 +8,7 @@ import Report from './Report';
 import Confirmation from './Confirmation';
 import { ArrowUturnLeftIcon } from "@heroicons/react/24/outline";
 import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { ForwardIcon } from "@heroicons/react/24/solid";
 import StepperIcon from './StepperIcon';
 import useWeeklyStore from '@/store/weeklyStore';
 import { showErrorNotification } from '@/utils/notifications';
@@ -15,7 +16,7 @@ import { showSuccessNotification } from '@/utils/notifications';
 
 export default function StepForm() {
   const router = useRouter()
-  const { validateTarget, validateContent, getWeeklyReportData, getWeeklyTargetData } = useWeeklyStore();
+  const { target, setTarget, clearData, validateTarget, validateContent, getWeeklyReportData, getWeeklyTargetData } = useWeeklyStore();
 
   //アクティブなステップの管理
   const [active, setActive] = useState(0);
@@ -25,7 +26,7 @@ export default function StepForm() {
       case 0:
         return {
           success: validateContent().success,
-          errorMessage: '1〜200文字で週間レポートを入力してください'
+          errorMessage: '1〜300文字で週間レポートを入力してください'
         };
       case 1:
         return {
@@ -48,24 +49,40 @@ export default function StepForm() {
 
   const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current));
 
+  const handleSkip = () => {
+    setActive(2);
+    setTarget(0);
+    showErrorNotification('目標の入力をスキップしました');
+  }
+
   const handleSubmit = async () => {
     const { weekly_report } = getWeeklyReportData();
     const { weekly_target } = getWeeklyTargetData();
     
     try {
-      const response = await axios.post(`/api/weeklyreport`, { weekly_report });
-
-      await axios.post(`/api/weeklytarget`, { weekly_target } )
-      showSuccessNotification(`登録しました`);
-      router.push('/dashboard');
-    } catch (error) {
-      showErrorNotification('送信に失敗しました。もう一度お試しください。');
-      console.error("Failed to fetch", error);
+      await axios.post(`/api/weeklyreport`, { weekly_report });
+    } catch(error) {
+      showErrorNotification('レポートの登録に失敗しました。もう一度お試しください。');
+      console.error("Failed to send weekly report", error);
+      return;
     }
-  };
 
-  //入力内容の送信状態
-  const [isSubmitted, setIsSubmitted] = useState(false);
+    try {
+      if (target !== 0) {
+        await axios.post(`/api/weeklytarget`, { weekly_target });
+      }
+    } catch (error) {
+      showSuccessNotification(`レポートを登録しました`);
+      showErrorNotification('お手数ですが目標設定ページより再度登録してください', '目標の登録に失敗しました');
+      console.error("Failed to send weekly target", error);
+      router.push('/dashboard');
+      clearData();
+      return; 
+    }
+    showSuccessNotification(`登録しました`);
+    router.push('/dashboard');
+    clearData();
+  };
 
   return (
     <>
@@ -93,15 +110,26 @@ export default function StepForm() {
         </div>
 
         {/* ボタン */}
-        <div className="flex justify-center md:justify-end mt-4 gap-3 md:px-4">
-          { active > 0 && active < 3 && (
+        <div className='flex flex-col md:flex-row justify-center pt-2'>
+
+          { active == 1 && (
+            <div className="flex justify-center md:justify-end mt-4">
+              <Button size="sm" variant="outline" color="#9ca3af" onClick={handleSkip}>
+                <ForwardIcon className="w-4 h-4 mr-1 text-gray-400" />
+                目標設定をスキップする
+              </Button>
+            </div>
+          )}
+
+          <div className="flex flex-row justify-center mt-4 gap-3 md:px-4">
+          { active > 0 && (
             <Button size="sm" variant="outline" color="#9ca3af" onClick={prevStep}>
               <ArrowUturnLeftIcon className="w-4 h-4 mr-1 text-gray-400" />
               戻る
             </Button>
           )}
 
-          {!isSubmitted && (active < 2 ? (
+          {active < 2 ? (
             <Button size="sm" variant="outline" color="gray" onClick={nextStep}>
               次へ
               <PaperAirplaneIcon className="w-5 h-5 ml-1 text-blue-400" />
@@ -111,14 +139,8 @@ export default function StepForm() {
               登録
               <PaperAirplaneIcon className="w-5 h-5 ml-1 text-blue-400" />
             </Button>
-          ))}
-
-          {/* {isSubmitted && (
-            <Button size="sm" variant="outline" color="#9ca3af" onClick={prevStep}>
-              <ArrowUturnLeftIcon className="w-4 h-4 mr-1 text-gray-400" />
-              ダッシュボードに戻る
-            </Button>
-          )} */}
+          )}
+        </div>
         </div>
       </div>
     </>
