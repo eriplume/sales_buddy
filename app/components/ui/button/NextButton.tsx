5e873a081d732531b610816d2c67970855e7d0d0
eriplume <eripi840@gmail.com>
2023-12-24 18:47:23 +0900

    :bug: Fix: ボタンのtypeプロパティを削除 #40

--- app/components/ui/button/NextButton.tsx
+++ app/components/ui/button/NextButton.tsx
@@ -3,13 +3,12 @@ import { ChevronDoubleRightIcon } from "@heroicons/react/24/outline";
 
 type NextButtonProps = {
   size: "xs" | "sm" | "md" | "lg" | "xl";
-  type?: "submit";
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
-export default function NextButton({ size, type, onClick }: NextButtonProps) {
+export default function NextButton({ size, onClick }: NextButtonProps) {
   return (
-    <Button size={size} type={type} variant="filled" color="rgba(147, 197, 253, 1)" onClick={onClick} className='hover:shadow-md'>
+    <Button size={size} variant="filled" color="rgba(147, 197, 253, 1)" onClick={onClick} className='hover:shadow-md'>
       次へ
       <ChevronDoubleRightIcon className="w-5 h-5 ml-2" />
     </Button>
