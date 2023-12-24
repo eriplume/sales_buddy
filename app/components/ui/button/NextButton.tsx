3b45e86f035426bbef4feced812c461b915f1fc5
eriplume <eripi840@gmail.com>
2023-12-24 18:04:54 +0900

    :bug: Fix: ボタンの色が反映されない問題 #40

--- app/components/ui/button/NextButton.tsx
+++ app/components/ui/button/NextButton.tsx
@@ -9,7 +9,7 @@ type NextButtonProps = {
 
 export default function NextButton({ size, type, onClick }: NextButtonProps) {
   return (
-    <Button size={size} type={type} variant="filled" color="#93c5fd" onClick={onClick} className='hover:shadow-md'>
+    <Button size={size} type={type} variant="filled" color="rgba(147, 197, 253, 1)" onClick={onClick} className='hover:shadow-md'>
       次へ
       <ChevronDoubleRightIcon className="w-5 h-5 ml-2" />
     </Button>
