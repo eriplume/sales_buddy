1f592713616a28b3fe045f5795578cbecc889313
eriplume <eripi840@gmail.com>
2023-12-27 19:17:30 +0900

    :sparkles: Feat: パンくずでの画面遷移追加 #46

--- app/(authenticated)/archive/page.tsx
+++ app/(authenticated)/archive/page.tsx
@@ -1,10 +1,14 @@
 import MonthlyArchive from "@/app/components/page/Archive/MonthlyArchive"
+import Breadcrumbs from "@/app/components/ui/Breadcrumbs"
 
 export default function Archive() {
 
   return (
-    <div className="flex flex-col justify-center items-center mx-auto px-6 z-0 max-w-4xl my-10 xl:my-20">
-      <MonthlyArchive />
-    </div>
+    <>
+      <Breadcrumbs>archive</Breadcrumbs>
+      <div className="flex flex-col justify-center items-center mx-auto px-6 z-0 max-w-4xl mb-7 mt-4">
+        <MonthlyArchive />
+      </div>
+    </>
   )
 }
