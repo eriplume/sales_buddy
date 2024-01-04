318482e6da4ffeb49bfc9d23c594137d6d62c03a
eriplume <eripi840@gmail.com>
2024-01-04 22:28:27 +0900

    :sparkles: Feat: 通知ステータスの取得 #53

--- app/components/page/Setting/AlertSelect.tsx
+++ app/components/page/Setting/AlertSelect.tsx
@@ -1,11 +1,24 @@
 "use client"
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Radio, Group, Button} from '@mantine/core';
 import { TriangleIcon } from '../../ui/icon/Triangle';
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
 
 export default function AlertSelect() {
   const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    const fetchNotifications = async () => {
+      try {
+        const response = await fetch(`/api/setting`);
+        const data = await response.json();
+        setChecked(data.notifications);
+      } catch (error) {
+        console.error('通知設定の取得に失敗しました', error);
+      }
+    };
+    fetchNotifications();
+  }, []);
 
   const label = () => {
     return (
@@ -20,22 +33,24 @@ export default function AlertSelect() {
 
   return (
     <>
-    <Radio.Group
-      name="select"
-      label={label()}
-      description="ONにすると毎週日曜日にお知らせします"
-    >
-      <Group mt="xs">
-        <Radio value="true" label="ON" color="#93c5fd"/>
-        <Radio value="false" label="OFF" color="#93c5fd"/>
-        <div className=''>
-      <Button variant='outline' size="xs" color='#9ca3af'>
-        更新
-        <ArrowPathIcon className="w-5 h-5 ml-2 text-blue-400" />
-    </Button>
-      </div>
-      </Group>
-    </Radio.Group>
+      <Radio.Group
+        name="select"
+        label={label()}
+        description="ONにすると毎週日曜日にお知らせします"
+        value={checked ? 'true' : 'false'}
+        onChange={(value) => setChecked(value === 'true')}
+      >
+        <Group mt="xs">
+          <Radio value="true" label="ON" color="#93c5fd"/>
+          <Radio value="false" label="OFF" color="#93c5fd"/>
+          <div>
+            <Button variant='outline' size="xs" color='#9ca3af'>
+              更新
+              <ArrowPathIcon className="w-5 h-5 ml-2 text-blue-400" />
+            </Button>
+          </div>
+        </Group>
+      </Radio.Group>
     </>
   )
 }
