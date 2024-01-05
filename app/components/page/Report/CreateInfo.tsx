888eef1d155eaab334fde240ff6c2edf5afa40cd
eriplume <eripi840@gmail.com>
2024-01-05 19:34:07 +0900

    :sparkles: Feat: 要約生成後最新のデータを取得し表示する #49

--- app/components/page/Report/CreateInfo.tsx
+++ app/components/page/Report/CreateInfo.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect, useCallback } from "react";
 import axios from 'axios'
 import { WeeklyReport } from '@/types';
+import useDashboardStore from "@/store/dashboardStore";
 import { Button } from '@mantine/core';
 import { PaperAirplaneIcon } from '@heroicons/react/24/outline';
 import { showErrorNotification, showSuccessNotification } from '@/utils/notifications';
@@ -19,6 +20,7 @@ type SummaryData = {
 export default function CreateInfo({reportsList, targetMonth}: CreateInfoProps) {
   const [isLoading, setIsLoading] = useState(false);
   const [summaryData, setSummaryData] = useState<SummaryData | null>(null);
+  const fetchMonthlyReport = useDashboardStore((state) => state.fetchMonthlyReport);
 
   const combinedReportsContent = reportsList.map(report => report.content).join("\n\n");
 
@@ -29,8 +31,7 @@ export default function CreateInfo({reportsList, targetMonth}: CreateInfoProps) 
         const response = await axios.post(`/api/summary`, { prompt: combinedReportsContent });
         console.log((response.data))
         setSummaryData(response.data as SummaryData);
-        setIsLoading(false); 
-
+        setIsLoading(false);
       } catch (error) {
         showErrorNotification('作成に失敗しました');
         console.error("Failed", error);
@@ -50,6 +51,8 @@ export default function CreateInfo({reportsList, targetMonth}: CreateInfoProps) 
           },
         });
         showSuccessNotification(`月間レポートを登録しました`);
+        setSummaryData(null);
+        fetchMonthlyReport(true);
       } catch(error) {
         showErrorNotification('保存に失敗しました。');
         console.error("Failed to send weekly report", error);
@@ -63,28 +66,31 @@ export default function CreateInfo({reportsList, targetMonth}: CreateInfoProps) 
       saveSummary();
     }
   }, [isLoading, summaryData, saveSummary]);
-
+    
   return (
     <>
       <div className="flex flex-col justify-center items-center px-7 md:px-12">
-        <div className="flex flex-row justify-center items-center px-7 md:px-12">
-          <Button 
-            variant="outline" 
-            color="#9ca3af" 
-            className="shadow-md hover:translate-y-1 hover:text-sky-700 transition-transform"
-            onClick={handleSubmit}
-          >
-            Create
-            <PaperAirplaneIcon className="w-5 h-5 ml-1 text-blue-400" />
-          </Button>
+
+        {summaryData === null ? (
+          <div className="flex flex-row justify-center items-center px-7 md:px-12">
+            <Button 
+              variant="outline" 
+              color="#9ca3af" 
+              className="shadow-md hover:translate-y-1 hover:text-sky-700 transition-transform"
+              onClick={handleSubmit}
+            >
+              Create
+              <PaperAirplaneIcon className="w-5 h-5 ml-1 text-blue-400" />
+            </Button>
+          </div>
+        ) : (
+          null
+        )}
+
+        <div className="flex flex-row justify-center items-center px-2 md:px-7 mt-3">
+          {isLoading && <Loading size="sm" /> }
         </div>
-        <div className="flex flex-row justify-center items-center px-7 md:px-12 mt-3">
-          {isLoading ? (
-            <Loading size="xs" /> // ローディングコンポーネントを表示
-          ) : (
-            summaryData && <div>{summaryData.text}</div>
-          )}
-        </div>
+
       </div>
     </>
   )
