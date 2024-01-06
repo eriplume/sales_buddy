45d93b51e5d4806291424dad9c7e9ebf52976e9c
eriplume <eripi840@gmail.com>
2024-01-06 18:00:11 +0900

    :lipstick: Update: Helpページの文言修正 #84

--- app/components/page/DairyRecord/HelpPage.tsx
+++ app/components/page/DairyRecord/HelpPage.tsx
@@ -6,34 +6,30 @@ export default function HelpPage() {
       <div className="p-2">
         <div className="flex items-center text-gray-700 underline">
           <CheckBadgeIcon className="w-6 h-6 text-blue-300 mr-2 font-bold" />
-          入力エリア
+          STEP1. 日付の選択
         </div>
         <div className="py-1 ml-8 text-sm text-gray-700 pb-2">
-          レシートごとに、フォームに沿って各値を入力してください。すべて入力必須です。
+          <p>登録する日付を選択。</p>
+          <p>既に登録済みの日は選択できません。</p>
         </div>
 
         <div className="flex items-center text-gray-700 border-t underline pt-2">
           <CheckBadgeIcon className="w-6 h-6 text-blue-300 mr-2 font-bold" />
-          加算するボタン
+          STEP2. 入力エリア
         </div>
         <div className="py-1 ml-8 text-sm text-gray-700 pb-2">
-          ボタンを押すと現在の合計値に入力値が加算されます。入力→加算ボタンをレシート分繰り返してください。
+          <p>フォームに沿ってレシートの内容を入力してください。</p>
+          <p>加算ボタンを押すと現在の合計値に加算されます。</p>
+          <p>レシート分こちらを繰り返します。</p>
         </div>
 
         <div className="flex items-center text-gray-700 border-t underline pt-2">
           <CheckBadgeIcon className="w-6 h-6 text-blue-300 mr-2 font-bold" />
-          登録ボタン
+          STEP3. 現在の合計値エリア
         </div>
         <div className="py-1 ml-8 text-sm text-gray-700 pb-2">
-          日付, 現在の合計値を確認したら登録ボタンを押して売上データを登録！登録したデータはダッシュボードのカレンダーから確認できます。 
-        </div>
-
-        <div className="flex items-center text-gray-700 border-t underline pt-3">
-          <CheckBadgeIcon className="w-6 h-6 text-blue-300 mr-2 font-bold" />
-          クリアボタン
-        </div>
-        <div className="py-1 ml-8 text-sm text-gray-700 pb-2">
-          現在の合計値がリセットされます。
+          <p>入力内容を確認して登録！</p>
+          <p>クリアボタンを押すと現在の合計値がリセットされます。</p>
         </div>
       </div>
     </>
