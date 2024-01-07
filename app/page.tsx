176e9965b3b77ef455f57c4c2fbc8bcfecb50656
eriplume <eripi840@gmail.com>
2024-01-08 00:32:47 +0900

    :lipstick: Add: topページレイアウト #55

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Auth from "@/lib/Auth";
 import Footer from "./components/base/Footer";
+import Introduction from "./components/page/Top/Introduction";
 
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center min-h-screen">
+      <div className="flex flex-col justify-center items-center mx-auto px-6 z-0 max-w-4xl mb-7 mt-4">
         <Auth/>
-        <p>トップページです</p>
+        <Introduction />
       </div>
       <Footer />
     </>
