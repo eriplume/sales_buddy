8df4c2aff803ddddae7c6febcee49c3641ea1ce6
eriplume <eripi840@gmail.com>
2024-01-09 14:06:25 +0900

    :bug: Fix: スマホ時のヘッダーレイアウト崩れ修正

--- app/components/base/Header.tsx
+++ app/components/base/Header.tsx
@@ -5,7 +5,7 @@ import DrawerMenu from "./DrawerMenu";
 
 export default function Header() {
   return (
-    <header className="md:sticky top-0 text-gray-600 body-font bg-white z-5 items-center pb-8 md:pb-0">
+    <header className="md:sticky top-0 text-gray-600 body-font bg-white z-5 items-center md:pb-0">
       <div className="container mx-auto flex flex-wrap p-5 items-center justify-between md:max-w-2xl lg:max-w-4xl relative">
         <div className="flex items-center">
           <div className="flex flex-row items-center">
