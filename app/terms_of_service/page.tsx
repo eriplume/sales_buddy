2e151a97ff63d42d59606c3fb469521e73d187fd
eriplume <eripi840@gmail.com>
2024-01-12 12:57:50 +0900

    :sparkles: Feat: 利用規約, プライバシーポリシー, お問い合わせを追加 #56

---
+++ app/terms_of_service/page.tsx
@@ -0,0 +1,9 @@
+import TermsOfService from '../components/base/TermsOfService'
+
+export default function page() {
+  return (
+  <div className="flex flex-col justify-center items-center mx-auto px-6 z-0 max-w-4xl mb-7 mt-4">
+    <TermsOfService />
+  </div>
+  )
+}
