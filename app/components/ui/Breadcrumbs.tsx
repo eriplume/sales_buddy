169f982973c1cb6aa13d3578eb2ae83b2c9f8fa5
eriplume <eripi840@gmail.com>
2024-01-14 15:37:35 +0900

    :lipstick: Update: UI編集 #97

--- app/components/ui/Breadcrumbs.tsx
+++ app/components/ui/Breadcrumbs.tsx
@@ -23,10 +23,10 @@ export function BreadcrumbsDemo({ children }: BreadcrumbsProps) {
     <div className='flex flex-row items-center px-2 pt-4 max-w-xs md:max-w-lg mx-auto'>
       <Link href='/sample/dashboard' className='flex flex-row text-gray-400 items-center hover:underline hover:text-sky-800 cursor-pointer'>
         <HomeIcon className="w-4 h-4 mr-1" />
-        <div>ダッシュボード sample</div>
+        <div className='text-xs'>ダッシュボード sample</div>
       </Link>
       <div className='text-xs text-gray-400 mx-2'>＜</div>
-      <div className='text-sky-700 cursor-pointer'>{children}</div>
+      <div className='text-sky-700 cursor-pointer text-xs'>{children}</div>
     </div>
   )
 }
