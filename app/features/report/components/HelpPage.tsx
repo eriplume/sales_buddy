0206f787b015b3c479d8a56757a7a5c57447781d
eriplume <eripi840@gmail.com>
2024-01-17 07:38:27 +0900

    :zap: Update: ヘルプ文言追加 #106

--- app/features/report/components/HelpPage.tsx
+++ app/features/report/components/HelpPage.tsx
@@ -19,13 +19,14 @@ export default function HelpPage() {
         <div className="py-1 ml-8 text-sm text-gray-700 pb-2">
           <p>週間レポートを要約し月間レポートを自動で作成します。</p>
           <p className="text-red-400">⚠️ 月間レポートの作成には3週分以上の週間レポートが必要です。</p>
+          <p className="text-red-400">⚠️ AIによる要約の精度にばらつきがある可能性がございます。</p>
         </div>
         <div className="flex items-center text-gray-700 underline border-t pt-2">
           <CheckBadgeIcon className="w-6 h-6 text-blue-300 mr-2 font-bold" />
           月間レポートの編集について
         </div>
         <div className="py-1 ml-8 text-sm text-gray-700 pb-2">
-          <p>AIが作成した文章を元に300字以内で編集可能です。</p>
+          <p>AIが作成した文章を元に400字以内で編集可能です。</p>
         </div>  
       </div>
     </>
