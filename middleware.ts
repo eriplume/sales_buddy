57c0c4605674744bcf1c9c44e0bf7f724d64f4ce
eriplume <eripi840@gmail.com>
2024-02-12 14:35:17 +0900

    :bug: Fix: middleware修正 #134
    アクセス制限パス修正

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/(dashboard|about|dairyrecord|setting|teams|dm_temas|weekly|admin)/:path*"],
+  matcher: ["/(dashboard|about|dairyrecord|setting|team|weekly|admin)/:path*"],
 };
